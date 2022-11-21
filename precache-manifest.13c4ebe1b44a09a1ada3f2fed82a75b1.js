b141cce461453ec183c2488d859b7095a6e35978
alifmufthi91 <alif.stinky@gmail.com>
2022-11-21 07:14:29 +0000

    Deploying to gh-pages from @ alifmufthi91/demo-walletconnect@95dd35d2f6b76183353a10c1d42b6a25707ddf43 🚀

---
+++ precache-manifest.13c4ebe1b44a09a1ada3f2fed82a75b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c1d478a1de2aa571c3",
+    "url": "/walletconnect-example-dapp/static/js/main.a0c1d478.chunk.js"
+  },
+  {
+    "revision": "ce3bf99024e24fa42c7d",
+    "url": "/walletconnect-example-dapp/static/js/1.ce3bf990.chunk.js"
+  },
+  {
+    "revision": "b9fb507d40c767f08e4e",
+    "url": "/walletconnect-example-dapp/static/js/runtime~main.b9fb507d.js"
+  },
+  {
+    "revision": "6c6f52b09af2c84c6b4f2f7ce488b645",
+    "url": "/walletconnect-example-dapp/static/media/algo.6c6f52b0.svg"
+  },
+  {
+    "revision": "d2de5f85099252ea528053c4228e2deb",
+    "url": "/walletconnect-example-dapp/index.html"
+  }
+];
